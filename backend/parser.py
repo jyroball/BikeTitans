340d160a821226eec8a1f5c841d65a90868fb762
therealsamyak <therealsamyak@gmail.com>
2025-03-12 20:11:50 -0700

    fixed backend parsing script

--- backend/parser.py
+++ backend/parser.py
@@ -42,9 +42,9 @@ def find_jpg_file(txt_file):
 
 # Function to extract the total number of slots from the image filename
 def extract_number_from_filename(filename):
-    match = re.search(r"([a-zA-Z_]+)_(\d+)\.(jpg|JPG)", filename)
+    match = re.search(r"(.+?)_(\d+)\.(jpg|JPG)$", filename)
     if match:
-        part_before_underscore = match.group(1)  # Title (before '_')
+        part_before_underscore = match.group(1).strip()  # Title (before '_')
         number_after_underscore = match.group(2)  # Total slots number (after '_')
         try:
             return part_before_underscore, int(number_after_underscore)
