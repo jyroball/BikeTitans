a8bdd4d327d3b9d6693b8c2b650965b85509cf0f
therealsamyak <therealsamyak@users.noreply.github.com>
2025-03-16 17:43:45 +0000

    deploy: 79d83901a0f6f2d96764ae8d927995cd310368f5

--- _app/immutable/chunks/NWVu-gNS.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1ds8j5p)==null?void 0:s.base)??"/BikeTitans";var a;const t=((a=globalThis.__sveltekit_1ds8j5p)==null?void 0:a.assets)??e;export{t as a,e as b};
