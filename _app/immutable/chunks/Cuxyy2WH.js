9ae6c99674767ac2a5ac12873b8d5a520dfb8fff
therealsamyak <therealsamyak@users.noreply.github.com>
2025-03-30 05:23:42 +0000

    deploy: b8e36f2372f7362875fe4c21dbfefb5d529db626

--- _app/immutable/chunks/Cuxyy2WH.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_2h5lva)==null?void 0:s.base)??"/BikeTitans";var a;const t=((a=globalThis.__sveltekit_2h5lva)==null?void 0:a.assets)??e;export{t as a,e as b};
