5afa88831c8cb1ec75f59976e3f8d5266807b580
therealsamyak <therealsamyak@users.noreply.github.com>
2025-04-17 20:15:56 +0000

    deploy: b8e36f2372f7362875fe4c21dbfefb5d529db626

--- _app/immutable/chunks/GsjKUilg.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_35eaii)==null?void 0:s.base)??"/BikeTitans";var a;const i=((a=globalThis.__sveltekit_35eaii)==null?void 0:a.assets)??e;export{i as a,e as b};
---
+++ _app/immutable/entry/start.CbO3RLs0.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DGgXt5Jx.js";export{o as load_css,r as start};
